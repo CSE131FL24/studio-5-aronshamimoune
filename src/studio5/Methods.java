d9599e1e2c90f053cdd3e5744d38e8f60df9bd9e
harri <harri@HarrisonZenbook.dhcp.wustl.edu>
2024-10-17 12:38:36 -0500

    finished studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,9 +14,9 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+
 		return distance;
 	}
 
@@ -30,73 +30,101 @@ public class Methods {
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(x, y, (3.0/4.0)*radius);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(x, y, (1.0/2.0)*radius);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(x, y, (1.0/4.0)*radius);
+	}
 
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
-		
 
-		// Red ring with 1.0/2.0 the radius
-		// suggested rgb values: 146, 0, 0
 
-		
+			// Red ring with 1.0/2.0 the radius
+			// suggested rgb values: 146, 0, 0
 
-		// Yellow ring with 1.0/4.0 the radius
-		// suggested rgb values: 255, 255, 109
 
-		
-	}
 
-	/**
-	 * Return a new String which is the original source String with all occurrences
-	 * of the target character substituted by the replacement String.
-	 * 
-	 * @param source      the source String
-	 * @param target      the target character to be replaced
-	 * @param replacement the replacement String
-	 * 
-	 * @return the String which results from substituting all of the target
-	 *         characters in the source String with the replacement String
-	 */
-	public static String substituteAll(String source, char target, String replacement) {
-		String result = "";
-		// TODO: Finish this method
-		
-		return result;
-	}
+				// Yellow ring with 1.0/4.0 the radius
+				// suggested rgb values: 255, 255, 109
 
-	/**
-	 * Compute the sum of elements in an array
-	 * 
-	 * @param values an array of integers
-	 * @return the sum of the elements in values
-	 */
-	public static int arraySum(int[] values) {
-		int sum = 0;
-		// FIXME: Compute the sum of the values in an array
-		
-		return sum;
-	}
 
-	/**
-	 * Return an array of a given size filled with the provided value
-	 * 
-	 * @param length the length of the returned array
-	 * @param value  the value to fill the array with
-	 * @return and array of size that's filled with value
-	 */
-	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
-		// TODO: Finish this method
 
-		
+			/**
+			 * Return a new String which is the original source String with all occurrences
+			 * of the target character substituted by the replacement String.
+			 * 
+			 * @param source      the source String
+			 * @param target      the target character to be replaced
+			 * @param replacement the replacement String
+			 * 
+			 * @return the String which results from substituting all of the target
+			 *         characters in the source String with the replacement String
+			 */
+			public static String substituteAll(String source, char target, String replacement) {
+				String result = "";
+				// TODO: Finish this method
 
-		return values;
-	}
+				return result;
+			}
 
-	// TODO: Create an arrayMean method which accepts an int array of values parameter.
-	// TODO: Create a JavaDoc comment for the arrayMean method.
+			/**
+			 * Compute the sum of elements in an array
+			 * 
+			 * @param values an array of integers
+			 * @return the sum of the elements in values
+			 */
+			public static int arraySum(int[] values) {
+				int sum = 0;
+				for (int i = 0; i < values.length; i++) {
+					sum = sum + values[i];
+				}
+				// FIXME: Compute the sum of the values in an array
 
-	
-}
+				return sum;
+			}
+
+			/**
+			 * Return an array of a given size filled with the provided value
+			 * 
+			 * @param length the length of the returned array
+			 * @param value  the value to fill the array with
+			 * @return and array of size that's filled with value
+			 */
+			public static int[] filledArray(int length, int value) {
+				int[] values = new int [length]; // FIXME: Create an array of the appropriate size
+				// TODO: Finish this method
+				for (int i = 0; i < values.length; i++) {
+					values[i] = value;
+				}
+
+
+
+				return values;
+			}
+
+			// TODO: Create an arrayMean method which accepts an int array of values parameter.
+			
+			/**
+			 * Compute the mean of the sum of elements in an array
+			 * 
+			 * @param values an array of integers
+			 * @return the mean of sum of the elements in values
+			 */
+			
+			public static double arrayMean(int[] values) {
+				double sum = 0;
+				for (int i = 0; i < values.length; i++) {
+					sum = sum + values[i];
+				}
+				return (sum/values.length);
+
+			}
+			// TODO: Create a JavaDoc comment for the arrayMean method.
+
+
+		}
